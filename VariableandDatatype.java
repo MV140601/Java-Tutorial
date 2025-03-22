184340e25b3d0f71dc0ebaed08b52e0181eecc7b
Mayuresh Veeramallu <veeramallumayuresh@gmail.com>
2025-03-22 13:41:53 +0530

    variables and datatypes

---
+++ VariableandDatatype.java
@@ -0,0 +1,15 @@
+public class VariableandDatatype {
+    public static void main(String[] args) {
+        int age = 14;
+        boolean test = true;
+        int[] arr = { 1, 2, 3, 4, 5 };
+        System.out.println(age);
+        if (test) {
+            for (int i = 0; i < arr.length; i++) {
+                System.out.println(arr[i]);
+                ;
+
+            }
+        }
+    }
+}
